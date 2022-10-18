3246031eb37ec6ea0acb988706340fa6805af9e7
Flaviotix <[flavionannini@gmail.com]>
2022-10-18 17:58:03 +0200

    navigation

---
+++ src/pages/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est,
+        reiciendis. Doloribus, doloremque nihil beatae dolorem asperiores
+        cupiditate molestiae fugit consectetur id, iste, quam repudiandae
+        explicabo porro quia. Quas recusandae non aliquid est aliquam,
+        temporibus pariatur consequatur quos cupiditate, quam fugit saepe? Neque
+        optio similique reprehenderit, facere ipsum doloremque impedit itaque
+        quae eum illo minus consequuntur officiis error fugiat ullam fugit
+        voluptatibus eveniet possimus nemo molestias. Odit ea ab blanditiis
+        quae! Consequatur commodi hic asperiores omnis tempora ducimus incidunt
+        animi nihil officiis suscipit. Dicta culpa dolorem quod inventore sequi
+        soluta at, minima, error perspiciatis, quisquam saepe tenetur impedit
+        aspernatur neque? Assumenda.
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque culpa,
+        perferendis odit quibusdam itaque minus quam quae cumque maiores
+        inventore, nobis delectus, laudantium dolorem nostrum quo minima
+        voluptatibus quos accusamus esse necessitatibus facere perspiciatis
+        aliquid praesentium. Sed assumenda labore eius ducimus? Sunt non saepe
+        corporis minima porro dolorem deserunt dolore laudantium nihil officiis
+        unde natus facilis maxime cumque minus sint veritatis enim, animi
+        repellendus voluptate, quas culpa perferendis velit ex? Voluptate quae
+        cupiditate saepe ad minus! Quod possimus unde ducimus voluptate
+        veritatis necessitatibus laudantium molestias expedita, in, libero
+        dolore qui aliquam, et error sit at pariatur voluptatibus assumenda quos
+        nihil!
+      </p>
+    </div>
+  );
+};
+
+export default About;
