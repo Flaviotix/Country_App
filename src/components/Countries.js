16d633f41383f443f7ad1216540871bb71d68159
Flaviotix <[flavionannini@gmail.com]>
2022-10-20 17:12:01 +0200

    card

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
+
   return (
     <div className="countries">
-      <h1>COUNTRIES</h1>
+      <ul>
+        {data.map((country, index) => (
+          <Card country={country} key={index} />
+        ))}
+      </ul>
     </div>
   );
 };
