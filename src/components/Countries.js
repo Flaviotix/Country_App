5bcc93ee325c458fccae62a9d74fbe89a4f2698c
Flaviotix <[flavionannini@gmail.com]>
2022-10-21 01:04:44 +0200

    Partie 1 terminé

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,10 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [rangeValue, setValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radio = ["Africa", "America", "Asia", "Europe", "Océania"];
+
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
@@ -12,10 +16,40 @@ const Countries = () => {
 
   return (
     <div className="countries">
-      <ul>
-        {data.map((country, index) => (
-          <Card country={country} key={index} />
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {radio.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={continent == selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card country={country} key={index} />
+          ))}
       </ul>
     </div>
   );
